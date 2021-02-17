46503c493bc608bf43512f2084b27cc0ff81d7f1
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-02-17 09:29:32 -0700

    Add initial form jsx

--- src/App/App.js
+++ src/App/App.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: [],
+    }
+  }
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <form>
+            <input placeholder='Name' type='text'/>
+            <input type='date' placeholder='mm/dd'/>
+            <input type='time'/>
+            <input type='number'placeholder='Number of guests'/>
+            <button>Make Reservation</button>
+          </form>
         </div>
         <div className='resy-container'>
-          
+
         </div>
       </div>
     )
